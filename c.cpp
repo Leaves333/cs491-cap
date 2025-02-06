2ea13ddce606200f23634c63922682023a441c20
Leaves333 <canchenli@gmail.com>
2025-02-05 21:38:42 -0600

    failed attempt at bsts c

--- c.cpp
+++ c.cpp
@@ -12,11 +12,76 @@ typedef vector<vi> vvi;
 typedef vector<ll> vll;
 typedef vector<vll> vvll;
 
+const int NUM_VERTICES = 1001;
+vi values(NUM_VERTICES);
+vi parent(NUM_VERTICES);
+vvi children(NUM_VERTICES);
+
+int dfs(int root, bool find_min) {
+    int ret = values[root];
+    for (auto child : children[root]) {
+        if (find_min)
+            ret = min(ret, values[child]);
+        else
+            ret = max(ret, values[child]);
+        dfs(child, find_min);
+    }
+    return ret;
+}
+
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
-    int t; cin >> t;
-    while (t--) {
-        
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> values[i];
+    }
+
+    for (int i = 2; i <= n; i++) {
+        int p; cin >> p;
+        parent[i] = p;
+        children[p].push_back(i);
+    }
+
+    while (m--) {
+
+        char op;
+        int v, k;
+        cin >> op >> v >> k;
+
+        int extreme = dfs(v, (op == '-'));
+        int p = parent[v];
+        int parent_value = values[p];
+        while (true) {
+            if (p == 0) {
+                parent_value = -1;
+                break;
+            } else if (op == '-' && parent_value > values[v]) {
+                p = parent[p];
+                parent_value = values[p];
+            } else if (op == '+' && parent_value < values[v]) {
+                p = parent[p];
+                parent_value = values[p];
+            } else {
+                break;
+            }
+        }
+
+        /*cout << "dfs on vertex " << v << "->" << values[v] << ", extreme=" << extreme << endl;*/
+        /*cout << "parent value = " << parent_value << endl;*/
+        /**/
+        if (parent_value == -1) {
+            cout << "YES" << endl;
+        } else if (op == '-' && values[v] > parent_value && extreme - k < parent_value) {
+            cout << "NO" << endl;
+        } else if (op == '+' && values[v] < parent_value && extreme + k > parent_value) {
+            cout << "NO" << endl;
+        } else {
+            cout << "YES" << endl;
+        }
+
+        /*cout << endl;*/
     }
 }
