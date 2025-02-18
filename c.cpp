98f49c432b163f1e8f1cc266fb16b14f2a9a8272
Leaves333 <canchenli@gmail.com>
2025-02-17 20:28:27 -0600

    also failed attempt at c

---
+++ c.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n; cin >> n;
+    int d[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> d[i][j];
+        }
+    }
+
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (d[i][k] != 0 && d[k][j] != 0)
+                    d[i][j] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << d[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
