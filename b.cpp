dbdd26dbfeb5930a8ae133ce0a73df3b9b7500f6
Leaves333 <canchenli@gmail.com>
2025-02-18 21:40:19 -0600

    fix b

--- b.cpp
+++ b.cpp
@@ -10,53 +10,67 @@ typedef vector<vi> vvi;
 typedef vector<ll> vll;
 typedef vector<vll> vvll;
 
+vector<string> word_v;
+int word_to_index(string s) {
+    for (int i = 0; i < word_v.size(); i++) {
+        if (word_v[i] == s)
+            return i;
+    }
+    word_v.push_back(s);
+    return word_v.size() - 1;
+}
+
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
     int n; cin >> n;
     vector<string> words(n);
-    set<string> unique_words;
     for (int i = 0; i < n; i++) {
         cin >> words[i];
-        unique_words.insert(words[i]);
+        word_to_index(words[i]);
     }
 
-    map<string, map<string, ll>> d;
-    for (auto a : unique_words) {
-        for (auto b : unique_words) {
+    ll d[n][n];
+    for (int a = 0; a < word_v.size(); a++) {
+        for (int b = 0; b < word_v.size(); b++) {
             d[a][b] = LLONG_MAX;
-            if (a[a.length() - 1] == b[0]) {
-                d[a][b] = a.length();
+            if (word_v[a][word_v[a].length() - 1] == word_v[b][0]) {
+                d[a][b] = word_v[a].length();
             }
         }
     }
 
-    for (auto k : unique_words) {
-        for (auto a : unique_words) {
-            for (auto b : unique_words) {
+    for (int k = 0; k < word_v.size(); k++) {
+        for (int a = 0; a < word_v.size(); a++) {
+            for (int b = 0; b < word_v.size(); b++) {
                 if (d[a][k] < LLONG_MAX && d[k][b] < LLONG_MAX)
                     d[a][b] = min(d[a][b], d[a][k] + d[k][b]);
             }
         }
     }
 
-    sort(words.begin(), words.end());
+
+    vi nums;
+    for (int i = 0; i < word_v.size(); i++) {
+        nums.push_back(i);
+    }
+
     bool good = true;
     ll ans = LLONG_MAX;
     while (good) {
-        ll cur = words[words.size() - 1].length();
+        ll cur = word_v[nums[nums.size() - 1]].length();
         bool possible_perm = true;
-        for (int i = 0; i < words.size() - 1; i++) {
-            if (d[words[i]][words[i+1]] == LLONG_MAX) {
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (d[nums[i]][nums[i+1]] == LLONG_MAX) {
                 possible_perm = false;
                 break;
             }
-            cur += d[words[i]][words[i+1]];
+            cur += d[nums[i]][nums[i+1]];
         }
         if (possible_perm) {
             ans = min(ans, cur);
         }
-        good = next_permutation(words.begin(), words.end());
+        good = next_permutation(nums.begin(), nums.end());
     }
 
     cout << ans << endl;
