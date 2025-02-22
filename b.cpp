4354e93b27308c6ff0683e0a071aed89d61fe670
Leaves333 <canchenli@gmail.com>
2025-02-22 11:59:41 -0600

    failed attempt at graphs 2 b

--- b.cpp
+++ b.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include <vector>
 using namespace std;
 
 #define all(x) begin(x), end(x)
@@ -16,8 +15,27 @@ struct edge {
     bool color;
 };
 
-bool dfs() {
-    return false;
+vector<vector<edge>> edges_true;
+vector<vector<edge>> edges_false;
+
+vector<vector<bool>> visited(2);
+int starting_loc;
+bool starting_color;
+
+bool dfs(int location, bool color) {
+    if (location == starting_loc && color != starting_color)
+        return false;
+    if (visited[color][location])
+        return true;
+
+    visited[color][location] = true;
+    bool status = true;
+    const vector<edge>& edges = (color ? edges_true[location] : edges_false[location]);
+    for (edge e : edges) {
+        status &= dfs(e.dest, e.color);
+    }
+
+    return status;
 }
 
 int main() {
@@ -26,8 +44,8 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<vector<edge>> edges_true(n + 1);
-    vector<vector<edge>> edges_false(n + 1);
+    edges_true.assign(n + 1, {});
+    edges_false.assign(n + 1, {});
     while (m--) {
         int p, q;
         string v1, v2;
@@ -47,12 +65,29 @@ int main() {
         }
     }
 
-    vector<vector<bool>> visited(2, vector<bool>(n+1));
+    visited[false].assign(n + 1, false);
+    visited[true].assign(n + 1, false);
+    bool good = true;
+
     for (int i = 1; i <= n; i++) {
+        starting_loc = i;
+        bool ok = false;
         if (!visited[false][i]) {
+            starting_color = false;
+            ok |= dfs(i, false);
         }
         if (!visited[true][i]) {
+            starting_color = true;
+            ok |= dfs(i, true);
         }
+        if (!ok)
+            good = false;
+    }
+
+    if (good) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
     }
 
 }
