886dfed9e656b1548f25db9049f3a2af9dc78f88
Leaves333 <canchenli@gmail.com>
2025-02-26 15:19:45 -0600

    failed attempt at c

--- c.cpp
+++ c.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #pragma optimize("O3")
 
 #include <bits/stdc++.h>
@@ -12,11 +13,62 @@ typedef vector<vi> vvi;
 typedef vector<ll> vll;
 typedef vector<vll> vvll;
 
+const ll MOD = 1000000007;
+
+ll solve(ll l, ll r) {
+
+    cout << "solve called with " << l << " " << r << endl;
+
+    ll ans = 0;
+    for (int i = 0; i <= 61; i++) {
+
+        ll size = 1ll << (i + 1);
+
+        ll lower_bound = l / size;
+        ll upper_bound = (r / size) + 1;
+
+        cout << "for size = " << size << " => " << i << "... " << endl;
+
+        cout << "lower_bound = " << lower_bound << ", upper_bound=" << upper_bound << endl;
+
+        // add complete segments
+        ans += ((upper_bound - lower_bound - 1) % MOD) * (size / 2);
+        ans %= MOD;
+        cout << upper_bound - lower_bound - 1 << " complete segments" << endl;
+
+        // handle edge case
+        if (upper_bound - lower_bound - 1 == 0) {
+            int smol = max(l, (lower_bound * size) + size / 2);
+            int big = min(r, (lower_bound * size) + size / 2);
+            // TODO: wait this isn't working
+            continue;
+        }
+
+        // add stuff on left
+        ll stuff_on_left = (lower_bound + 1) * size - l;
+        ans += min(stuff_on_left, size / 2);
+        ans %= MOD;
+        cout << min(stuff_on_left, size / 2) << " on left" << endl;
+
+        // add stuff on right
+        ll stuff_on_right = (r - (upper_bound * size));
+        ans += max(0ll, stuff_on_right - (size / 2));
+        ans %= MOD;
+        cout << max(0ll, stuff_on_right - (size / 2)) << " on right" << endl;
+
+        cout << endl;
+
+    }
+    return ans;
+}
+
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
     int t; cin >> t;
     while (t--) {
-        
+        ll l, r;
+        cin >> l >> r;
+        cout << solve(l, r) << endl;
     }
 }
