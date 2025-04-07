edb90e384db698c94456a824d4e720605dfcf40f
Leaves333 <canchenli@gmail.com>
2025-04-07 12:51:34 -0500

    struggle against tsp

--- i.cpp
+++ i.cpp
@@ -12,8 +12,8 @@ typedef vector<vi> vvi;
 typedef vector<ll> vll;
 typedef vector<vll> vvll;
 
-struct edge {
-    int dest, cost;
+struct matrix {
+    int arr[20][1 << 20];
 };
 
 // mask: bitmask of visited cities
@@ -21,29 +21,28 @@ struct edge {
 // n: total number of cities
 // edges: adjacency matrix of distances
 // dp: memoization of states
-ll tsp(int mask, int cur, int n, const vvll &edges, vvll &dp) {
+int tsp(int mask, int cur, int n, const vvi &edges, matrix &dp) {
 
     // base case: all cities visited
     if (mask == (1 << n) - 1)
         return 0;
 
     // this case is in our dp
-    if (dp[cur][mask] != -1)
-        return dp[cur][mask];
+    if (dp.arr[cur][mask] != -1)
+        return dp.arr[cur][mask];
 
-    ll ans = INT_MAX;
+    int ans = INT_MAX;
 
     // try visiting every unvisited city
     for (int i = 0; i < n; i++) {
         if ((mask & (1 << i)) == 0) {
-            if (edges[cur][i] == -1)
+            if (edges[cur][i] == INT_MAX)
                 continue;
             ans = min(ans, edges[cur][i] + tsp((mask | 1 << i), i, n, edges, dp));
         }
     }
 
-    /*cout << "tsp called with mask=" << mask << ", cur=" << cur << " returned " << ans << endl;*/
-    return dp[cur][mask] = ans;
+    return dp.arr[cur][mask] = ans;
 
 }
 
@@ -53,7 +52,7 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vvll edges(n, vll(n, -1));
+    vvi edges(n, vi(n, INT_MAX));
     while (m--) {
         int u, v, d;
         cin >> u >> v >> d;
@@ -63,8 +62,25 @@ int main() {
         edges[v][u] = d;
     }
 
-    vvll dp(n, vll(1 << n, -1));
-    ll ans = INT_MAX;
+    // floyd-warshall to generate distance array
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (edges[i][k] < INT_MAX && edges[k][j] < INT_MAX)
+                    edges[i][j] = min(edges[i][j], edges[i][k] + edges[k][j]); 
+            }
+        }
+    }
+
+    /*vvi dp(n, vi(1 << n, -1));*/
+    matrix dp;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 1 << n; j++) {
+            dp.arr[i][j] = -1;
+        }
+    }
+    
+    int ans = INT_MAX;
     for (int i = 0; i < n; i++) {
         ans = min(ans, tsp(1 << i, i, n, edges, dp));
     }
