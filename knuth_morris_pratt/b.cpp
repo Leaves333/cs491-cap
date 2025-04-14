01ec1fc3ccaaf2adee9943b8f0c39d4e82c8fb92
Leaves333 <canchenli@gmail.com>
2025-04-14 14:36:10 -0500

    solve kmp b

---
+++ knuth_morris_pratt/b.cpp
@@ -0,0 +1,36 @@
+#pragma optimize("O3")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+vi prefix_function(string s) {
+    int n = s.length();
+    vi pi(n);
+    for (int i = 1; i < n; i++) {
+        int j = pi[i-1];
+        while (j > 0 && s[i] != s[j])
+            j = pi[j-1];
+        if (s[i] == s[j])
+            j++;
+        pi[i] = j;
+    }
+    return pi;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    string s; cin >> s;
+    vi p = prefix_function(s);
+    cout << s.substr(0, s.length() - p.back()) << endl;
+
+}
