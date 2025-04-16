617c190199263e4f4bd606abac582d33e4fb43cf
Leaves333 <canchenli@gmail.com>
2025-04-16 16:36:54 -0500

    struggle with b

--- b.cpp
+++ b.cpp
@@ -36,8 +36,8 @@ ll pow_mod(ll x, ll n) {
 ll hashify(string s) {
     ll p_hash = 0, q_hash = 0;
     for (int i = 0; i < s.length(); i++) {
-        p_hash += (s[i] - 'a') * p_pow[i];
-        q_hash += (s[i] - 'a') * q_pow[i];
+        p_hash += (s[i] - 'A' + 1) * p_pow[i];
+        q_hash += (s[i] - 'A' + 1) * q_pow[i];
         p_hash %= MOD;
         q_hash %= MOD;
     }
@@ -50,20 +50,21 @@ int main() {
     // precompute powers of p and q
     p_pow[0] = 1;
     q_pow[0] = 1;
-    for (int i = 1; i <= N; i++) {
+    for (int i = 1; i < N; i++) {
         p_pow[i] = (p_pow[i-1] * p) % MOD;
         q_pow[i] = (q_pow[i-1] * q) % MOD;
     }
-    for (int i = 0; i <= N; i++) {
+    for (int i = 0; i < N; i++) {
         p_inverse[i] = pow_mod(p_pow[i], MOD - 2);
         q_inverse[i] = pow_mod(q_pow[i], MOD - 2);
     }
 
     int n, m;
-    unordered_set<ll> words;
+    cin >> n >> m;
+    unordered_set<ll> dictionary_words;
     for (int i = 0; i < m; i++) {
         string s; cin >> s;
-        words.insert(hashify(s));
+        dictionary_words.insert(hashify(s));
     }
 
     vector<string> tokens(n);
@@ -71,23 +72,107 @@ int main() {
         cin >> tokens[i];
     }
 
+    vector<ll> token_hashes(n + 1, 0); // hash each prefix of tokens
+    vector<ll> length(n + 1, 0);       // length of first n elements
+    for (int i = 0; i < n; i++) {
+        length[i + 1] = length[i] + tokens[i].length();
+
+        // shift current hash down
+        ll cur_hash = token_hashes[i];
+        ll cur_p = cur_hash % (1ll << 32);
+        ll cur_q = cur_hash >> 32;
+
+        // merge with the hash of the new token
+        ll token_hash = hashify(tokens[i]);
+        ll token_p = token_hash % (1ll << 32);
+        ll token_q = token_hash >> 32;
+        token_p = (token_p * p_pow[length[i]]) % MOD;
+        token_q = (token_q * q_pow[length[i]]) % MOD;
+
+        cur_p = (cur_p + token_p) % MOD;
+        cur_q = (cur_q + token_q) % MOD;
+        token_hashes[i + 1] = cur_p | (cur_q << 32);
+    }
+    
+    /*cout << "here's token hashes: " << endl;*/
+    /*for (int i = 0; i < token_hashes.size(); i++) {*/
+    /*    cout << i << " : " << hex << token_hashes[i] << dec << endl;*/
+    /*}*/
+    /*cout << "here's length" << endl;*/
+    /*for (int i = 0; i < length.size(); i++) {*/
+    /*    cout << i << " : " << length[i] << endl;*/
+    /*}*/
+
     // dp[i] = -1 if no sol considering first i tokens
     // else dp[i] = prev
     vi dp(n + 1, -1);
+    dp[0] = 0;
     vector<string> word(n + 1);
     for (int i = 0; i < n; i++) {
-        ll cur_hash = 0;
+
+        string cur_word = "";
+
+        // iterate backwards through tokens until we find a word with a solution
         for (int j = i; j >= 0; j--) {
-            ll cur_p = cur_hash % (1ll << 32);
-            ll cur_q = cur_hash >> 32;
 
-            ll token_hash = hashify(tokens[j]);
-            ll token_p = token_hash % (1ll << 32);
-            ll token_q = token_hash >> 32;
+            // update the word
+            cur_word = tokens[j] + cur_word;
 
-            cur_p *= p_pow[tokens[j].length()];
-            cur_q *= q_pow[tokens[j].length()];
+            // compute hash using prefix sum
+            ll top_hash = token_hashes[i + 1];
+            ll top_p = top_hash % (1ll << 32);
+            ll top_q = top_hash >> 32;
+
+            ll bottom_hash = token_hashes[j]; 
+            ll bottom_p = bottom_hash % (1ll << 32);
+            ll bottom_q = bottom_hash >> 32;
+
+            ll cur_p = (top_p - bottom_p);
+            if (cur_p < 0)
+                cur_p += MOD;
+            ll cur_q = (top_q - bottom_q);
+            if (cur_q < 0)
+                cur_q += MOD;
+            cur_p = (cur_p * p_inverse[length[j]]) % MOD;
+            cur_q = (cur_q * q_inverse[length[j]]) % MOD;
+            ll hash = cur_p | (cur_q << 32);
+
+            // continue if no solution
+            if (!dictionary_words.count(hash))
+                continue;
+            if (dp[j] < 0)
+                continue;
+
+            word[i + 1] = cur_word;
+            dp[i + 1] = j;
+
         }
+    }
+
+    /*cout << "here's words:" << endl;*/
+    /*for (int i = 0; i < word.size(); i++) {*/
+    /*    cout << i << " : " << word[i] << endl;*/
+    /*}*/
+    /*cout << "here's dps:" << endl;*/
+    /*for (int i = 0; i < dp.size(); i++) {*/
+    /*    cout << i << " : " << dp[i] << endl;*/
+    /*}*/
+    /*cout << endl;*/
+
+    if (dp.back() == -1) {
+        cout << -1 << endl;
+    } else {
+        int cur = dp.size() - 1;
+        vector<string> ans;
+        while (cur != 0) {
+            ans.push_back(word[cur]);
+            cur = dp[cur];
+        }
+        reverse(ans.begin(), ans.end());
+        for (auto x : ans) {
+            cout << x << " ";
+        }
+        cout << endl;
     }
 
 }
